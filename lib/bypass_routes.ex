1c47282bf8fec571ccb43c011428332ae1dcb112
Ciarán Walsh <git@ciaranwal.sh>
2017-07-31 12:03:44 +0100

    Use a fixed module name based on the test module, and remove the module after test execution.

--- lib/bypass_routes.ex
+++ lib/bypass_routes.ex
@@ -2,7 +2,7 @@ defmodule BypassRoutes do
   defmacro bypass_routes(bypass, opts \\ [], [do: block]) do
     default_plugs = build_default_plugs(opts)
 
-    module_name = String.to_atom("BypassRouter#{__CALLER__.line}")
+    module_name = Module.concat(__CALLER__.module, BypassRouter)
 
     quote do
       defmodule unquote(module_name) do
@@ -19,6 +19,11 @@ defmodule BypassRoutes do
           raise "No route matched the #{var!(conn).method} request to #{var!(conn).request_path}"
         end
       end
+
+      ExUnit.Callbacks.on_exit(fn ->
+        :code.purge(unquote(module_name))
+        :code.delete(unquote(module_name))
+      end)
 
       Bypass.expect(unquote(bypass), fn conn ->
         Bypass.pass(unquote(bypass))
